420ad99c2423895705b393dbc0b54cab9cd8f401
Kristi Gribble <kgribble4083@gmail.com>
2020-04-19 21:05:13 -0500

    db-config

--- data/db-config.js
+++ data/db-config.js
@@ -1,8 +1,7 @@
 const knex = require("knex")
+
 const knexfile = require("../knexfile.js")
 
 const environment = process.env.DB_ENV || "development"
 
-const db = knex(knexfile[environment])
-
-module.exports = db;
+module.exports = knex(knexfile[environment])
