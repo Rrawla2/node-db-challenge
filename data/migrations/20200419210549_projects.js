a1614d3351c978628caaff029cd465a516935672
Kristi Gribble <kgribble4083@gmail.com>
2020-04-19 21:45:27 -0500

    Added down function to drop tables if they exist

--- data/migrations/20200419210549_projects.js
+++ data/migrations/20200419210549_projects.js
@@ -57,5 +57,9 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("project_resource")
+    .dropTableIfExists("task")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("project")
 };
